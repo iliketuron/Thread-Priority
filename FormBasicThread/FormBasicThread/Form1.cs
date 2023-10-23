5b3cffd03f022b5763f7fa22cc32a052be8ff1ee
iliketuron <144678282+iliketuron@users.noreply.github.com>
2023-10-23 17:44:39 +0800

    Add files via upload

---
+++ FormBasicThread/FormBasicThread/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormBasicThread
+{
+    public partial class FrmBasicThread : Form
+    {
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            Console.WriteLine("-Thread Starts-");
+            ThreadStart delThread1 = new ThreadStart(MyThreadClass.Thread1);
+            ThreadStart delThread2 = new ThreadStart(MyThreadClass.Thread2);
+
+
+            Thread threadA = new Thread(delThread1);
+            threadA.Name = "Thread A Process";
+            Thread threadB = new Thread(delThread2);
+            threadB.Name = "Thread B Process";
+            Thread threadC = new Thread(delThread1);
+            threadC.Name = "Thread C Process";
+            Thread threadD = new Thread(delThread2);
+            threadD.Name = "Thread D Process";
+
+            threadA.Priority = ThreadPriority.Highest;
+            threadB.Priority = ThreadPriority.Normal;
+            threadC.Priority = ThreadPriority.AboveNormal;
+            threadD.Priority = ThreadPriority.BelowNormal;  
+
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+            threadD.Start();
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+            threadD.Join();
+
+            Console.WriteLine("-End of Threads-");
+            label1.Text = "- End of Threads -";
+
+        }
+    }
+}
+
+
+
